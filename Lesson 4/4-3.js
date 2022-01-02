8f0d3761d3b1d11437de83c1ea870193ba362048
HyunJung <crispylemon7120@gmail.com>
2022-01-02 19:31:27 +0900

    01-02

--- Lesson 4/4-3.js
+++ Lesson 4/4-3.js
@@ -1,3 +1,38 @@
-const arrayToList = (arr) => {
-    let list = [{value : }]
+//list라는 자료구조 
+function Node(data){
+    this.data = data;
+    this.next = null;
 }
+
+const LinkedList = function(){
+    let _length = 0;
+    let _head = null;
+}
+
+LinkedList.appendNode = function(data){
+    const node = new Node(data);
+    if(this._head == null){
+        this._head = node;
+    }
+    else{
+        let curr = this._head;
+
+        while(curr.next){
+            curr = curr.next;
+        }
+
+        curr.next = node;
+    }
+
+    this._length++;
+}
+
+const arrayToList = (arr) => {
+    const list = new LinkedList();
+    for(let i = 0; i <  arr._length; i++){
+        list.appendNode(arr[i]);
+    }
+    return list
+}
+
+console.log(arrayToList([1, 2, 3])._length)
