b9f2e567ba4ff0ac64a14c176f851dd2ca47ff36
HyunJung Oh <jenny7120@naver.com>
2022-03-27 19:14:22 +0900

    03-27

--- 인프런 연습/go/reduce.js
+++ 인프런 연습/go/reduce.js
@@ -1,6 +1,6 @@
 const reduce = (f, acc, iter) => {
   //첫번째 값을 꺼내서 acc로 지정
-  if(!iter){
+  if (!iter) {
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
   }
@@ -8,11 +8,10 @@ const reduce = (f, acc, iter) => {
     acc = f(acc, a);
   }
   return acc;
+};
 
-  }
- 
-const add = (a, b) => a + b
+const add = (a, b) => a + b;
 
-console.log(reduce(add, [1, 2, 3, 4, 5]))
+console.log(reduce(add, [1, 2, 3, 4, 5]));
 
-console.log()
+console.log();
